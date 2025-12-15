f063c50fe4b3ce435cd3a240b773100cfaace89b
Luis Fernando Quispe Puma <l.quispe.puma@isur.edu.pe>
2025-12-15 16:08:10 -0500

    Proyecto Creado

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace App_CrediVnzl
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace App_CrediVnzl
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
